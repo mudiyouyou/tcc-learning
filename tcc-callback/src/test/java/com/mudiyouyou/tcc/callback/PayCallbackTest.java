e23c2b7643016bf0cea35dce9c5ce858f62c00ed
cobrano1 <cobrano1@163.com>
2019-08-01 17:17:47 +0800

    try to code with tcc

---
+++ tcc-callback/src/test/java/com/mudiyouyou/tcc/callback/PayCallbackTest.java
@@ -0,0 +1,10 @@
+package com.mudiyouyou.tcc.callback;
+
+
+public class PayCallbackTest {
+
+    @Test
+    public void testPayCallbackSucess(){
+
+    }
+}
---
+++ tcc-order-interface/src/main/java/com/mudiyouyou/tcc/order/IOrderService.java
@@ -0,0 +1,8 @@
+package com.mudiyouyou.tcc.order;
+
+import org.dromara.hmily.annotation.Hmily;
+
+public interface IOrderService {
+    @Hmily
+    void payNotify();
+}
